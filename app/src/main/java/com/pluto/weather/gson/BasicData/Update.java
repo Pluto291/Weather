96d47570b551ba3a81cfbc41e7474eee3c7ba083
Pluto <2823197499@qq.com>
2019-10-01 12:07:10 +0800

    create beta

--- app/src/main/java/com/pluto/weather/gson/BasicData/Update.java
+++
@@ -1,9 +0,0 @@
-package com.pluto.weather.gson.BasicData;
-
-public class Update {
-    //当地时间
-    public String loc;
-    
-    //UTC时间
-    public String utc;
-}
--- app/src/main/java/com/pluto/weather/gson/WeatherNow.java
+++
@@ -1,8 +0,0 @@
-package com.pluto.weather.gson;
-
-import com.pluto.weather.gson.BaseData.Weather;
-import com.pluto.weather.gson.BasicData.Now;
-
-public class WeatherNow extends Weather {
-    public Now now;
-}
